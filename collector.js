403f1c00dd658bab44d3d8135ea82493969a5289
moonrailgun <moonrailgun@gmail.com>
2015-10-12 16:30:26 +0800

    数据采集
    ADD 数据采集完成
    ADD 多层迭代

--- collector.js
+++ collector.js
@@ -35,12 +35,13 @@ function RunTask(task) {
         var startURL = json.startURL;
         var timeout = json.timeout;
         var encoding = json.encoding;
+        //获取起始页数据并分析
         httpHelper.get(startURL, timeout, function (err, val) {
             if (err) {
                 console.error(err);
             }
             else {
-                AnalysisPage(val, json);
+                AnalysisRootPage(val, json);
             }
         }, encoding);
     });
@@ -74,29 +75,41 @@ function SavePic(url, dirpath) {
 }
 
 
-//解析页面
-function AnalysisPage(doc, opt) {
+//解析根页面
+function AnalysisRootPage(doc, opt) {
     console.time("AnalysisPage");
     var selectior = opt.selectior;
-    var title = selectior.title;
     var content = selectior.content;
-    var $ = cheerio.load(doc, {decodeEntities: false});
 
-    var pageTitle = $(title).html();
-    var pageContent = "";
-    if (typeof content == "string") {
-        $(content).text(function (index, oldcontent) {
-            pageContent += oldcontent.trim() + "\n";
-        });
-    }
-    else if (typeof content == "object") {
+    console.log("采集任务：" + opt.name);
+
+    //基本配置
+    var parent = {};
+    parent.url = opt.startURL;
+    parent.title = opt.name;
+    AnalysisSubPage(doc, content,parent, opt);//开始解析页面
+
+    console.timeEnd("AnalysisPage");
+}
+
+//分析页面，从当前页获取子页URL，title。然后进入下载子页获取内容
+//当前页面，内容选择器，任务设置
+//todo mulPage,joint,type处理
+function AnalysisSubPage(doc, content, parent, opt) {
+    var $ = cheerio.load(doc, {decodeEntities: false});
+    var parentUrl = parent.url;
+    var parentTitle = parent.title;
+
+    //解析内容
+    if (typeof content == "object") {
         var subURL = content.subURL;
         var subTitle = content.subTitle;
+
         var urlList = [];
         var par = subURL.split(" ");
-        if(par[par.length - 1] == "a"){
-            $(subURL).attr('href',function (index, oldcontent) {
-                urlList[index] = oldcontent;
+        if (par[par.length - 1] == "a") {
+            $(subURL).attr('href', function (index, oldcontent) {
+                urlList[index] = oldcontent;//将数据按顺序放到数组中
             });
         }
         var titleList = [];
@@ -104,28 +117,42 @@ function AnalysisPage(doc, opt) {
             titleList.push(oldcontent.trim());
         });
 
-        //查找子页内容
-        for(var i = 0;i<1;i++)
-        {
+        //循环遍历子页内容
+        for (var i = 0; i < urlList.length; i++) {
             var url = urlList[i];
-            httpHelper.get(url, opt.timeout, function (err, val,url) {
-                if (err) {
-                    console.error(err);
-                }
-                else {
-                    var $temp = cheerio.load(val, {decodeEntities: false});
-                    console.log($temp(content.subContent).html());
-                    console.log("--------------------------------------"+url);
-                }
-            }, opt.encoding);
+            if(url){
+                httpHelper.get(url, opt.timeout, function (err, val, url) {
+                    if (err) {
+                        console.error(err);
+                    }
+                    else {
+                        var parent = {};
+                        parent.url = url;
+                        parent.title = titleList[urlList.indexOf(url)];
+                        AnalysisSubPage(val, content.subContent, parent , opt);
+                    }
+                }, opt.encoding);
+            }
         }
-    }
+    } else if (typeof content == "string") {
+        var pageContent = "";
+        //如果是内容选择器是文本，则终止迭代，获取到的数据被回收
+        $(content).text(function (index, oldcontent) {
+            pageContent += oldcontent.trim() + "\n";
+        });
 
-    console.log(pageContent);
-    console.timeEnd("AnalysisPage");
+        console.log("-----------------------------" + parentUrl);
+        if(pageContent){
+            console.log("最终数据:"+ parentTitle + "\t\t\t---\t\t\t" +pageContent);
+        }else{
+            console.log("没有采集到任何数据");
+        }
+        console.log();
+    }
 }
 
 exports.LoadTask = LoadTask;
 exports.RunTask = RunTask;
 exports.SavePic = SavePic;
-exports.AnalysisPage = AnalysisPage;
+exports.AnalysisRootPage = AnalysisRootPage;
+exports.AnalysisSubPage = AnalysisSubPage;
